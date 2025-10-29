f5a9f5a72db3c116246aab80c45cd51782516bda
Shady <shadymutethia@gmail.com>
2025-10-29 19:25:04 +0300

    fix model

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
                     ],
                 },
             ],
-            { model: "gpt-4.1" }
+            { model: "claude-sonnet-4.5" }
         ) as Promise<AIResponse | undefined>;
     };
 
